ce6517e2d82cbaec6a10e5d5222cdf35b146dccd
Cody Le <codyle@csu.fullerton.edu>
2026-07-11 22:51:26 -0700

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashmap = {}
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+        
+        result = heapq.nlargest(k, hashmap.keys(), key=hashmap.get)
+        return result
