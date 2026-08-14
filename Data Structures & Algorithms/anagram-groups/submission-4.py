1d44ed576dd56871e8be21ab3be4f4532bd86e93
Cody Le <codyle@csu.fullerton.edu>
2026-08-14 10:06:28 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # {sorted string: list of anagrams}
+        hashmap = {}
+        result = []
+        for string in strs:
+            # sort the string.
+            sorted_string = "".join(sorted(string))
+            if sorted_string in hashmap:
+                # in hashmap, add string to value which is a list
+                hashmap[sorted_string].append(string)
+            else:
+                hashmap[sorted_string] = [string]
+            
+        result = list(hashmap.values())
+        return result
